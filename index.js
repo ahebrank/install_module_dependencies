7aa027a1bea9486c3b70468ebb743e86ebb5a904
Andy Hebrank <ahebrank@gmail.com>
2017-03-06 22:02:59 -0500

    delete submodule .gits

--- index.js
+++ index.js
@@ -40,6 +40,29 @@ function installPackages(packages) {
   });
 }
 
+function removeGit() {
+  var composerdir = root + '/vendor';
+  glob(composerdir + '/**/.git', {}, (err, files) => {
+    files.forEach((fn) => {
+     deleteFolderRecurise(fn);
+    });
+  });
+}
+
+function deleteFolderRecursive(path) {
+  if( fs.existsSync(path) ) {
+    fs.readdirSync(path).forEach(function(file,index){
+      var curPath = path + "/" + file;
+      if(fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+}
+
 var packages = [];
 glob(moduleDir + '/**/composer.json', {}, (err, files) => {
   files.forEach((fn) => {
